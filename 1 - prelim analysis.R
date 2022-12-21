a39b73da1cdf5fd3ad4b33ed45e759477a34b255
amandabhernandez <amandabhernandez@gmail.com>
2022-12-21 14:29:53 -0600

    update with conservative subset comparison analysis

--- 1 - prelim analysis.R
+++ 1 - prelim analysis.R
@@ -12,6 +12,7 @@ p_load(tidyverse)
 p_load(janitor)
 p_load(broom)
 p_load(sf)
+p_load(flextable)
 
 options (stringsAsFactors = FALSE)
 
@@ -25,11 +26,8 @@ setwd(source_file_loc)
 
 load("census_eji_all_ppps.RData")
 
-### load csv from CDC site
-eji_data_fromCDC <- read_csv("eji/DataRecords.csv")
 
-
-setdiff( colnames(sites_census_tract), colnames(census_tracts))
+# setdiff(colnames(sites_census_tract), colnames(eji_spatial_tidy))
 
 
 
@@ -37,42 +35,74 @@ setdiff( colnames(sites_census_tract), colnames(census_tracts))
 # 2. MERGE EJI AND PPPS ####
 ################################################################################
 
-ppps_census <- st_drop_geometry(sites_census_tract) %>% 
+ppps_salvatore_tract_df <- st_drop_geometry(ppps_salvatore_tract) %>% 
   distinct() %>% 
   select(dataset:geoid)
 
-ppps_count <- ppps_census %>% 
-  group_by(state, county, geoid) %>% 
-  count()
+ppps_eh249_tract_df <- st_drop_geometry(ppps_eh249_tract) %>% 
+  distinct() %>% 
+  select(dataset:geoid)
 
+ppps_salvatore_count <- ppps_salvatore_tract_df %>% 
+  group_by(geoid) %>% 
+  count() %>% 
+  filter(!is.na(geoid))
 
-census_tracts_df <- st_drop_geometry(census_tracts)  %>% 
+ppps_eh249_count <- ppps_eh249_tract_df %>% 
+  group_by(geoid) %>% 
+  count() %>% 
+  filter(!is.na(geoid))
+
+eji_tracts_df <- st_drop_geometry(eji_spatial_tidy)  %>% 
   distinct()
 
-#how many census tracts with PFAS point source data
-length(unique(ppps_count$geoid))
-#20,354 census tracts with PFAS point source data 
+
 
 #how many census tracts total
-length(unique(census_tracts_df$geoid))
+length(unique(eji_tracts_df$geoid))
 #84,414 census tracts 
+#73,056 census tracts
 
 #how many census tracts in EJI
 length(unique(eji_data_fromCDC$geoid))
 #73,868
+#71,677 (12/21/22) 
+
+#how many census tracts with PFAS point source data
+length(unique(ppps_salvatore_count$geoid))
+#16,274 census tracts with PFAS point source data
+
+#how many census tracts with PFAS point source data
+length(unique(ppps_eh249_count$geoid))
+#20,354 census tracts with PFAS point source data 
+#25,681 census tracts with PFAS point source data (12/21/22) -- diff likely bc 
+#we fixed the census tract matching issue
+
 
 #as a percent? 
-(length(unique(ppps_count$geoid))/length(unique(eji_data_fromCDC$geoid)))*100
+(length(unique(ppps_eh249_count$geoid))/length(unique(eji_data_fromCDC$geoid)))*100
+#23%
+#35.8%
+
+
+#as a percent? 
+(length(unique(ppps_salvatore_count$geoid))/length(unique(eji_data_fromCDC$geoid)))*100
+#22%
+
+#how many facilities in ppps dataset?
+sum(ppps_eh249_count$n)
+#83,692
+#110,691 (12/21/22)
 
 
 #how many facilities in ppps dataset?
-sum(ppps_count$n)
-#83,692
+sum(ppps_salvatore_count$n)
+#32,125 (12/21/22)
 
 
 
-all_tracts_ppps <-eji_data_fromCDC %>% 
-  full_join(ppps_count)  %>% 
+eji_w_ppps_eh249 <-eji_data_fromCDC %>% 
+  left_join(ppps_eh249_count)  %>% 
   select(1:geoid, contains("EJI"), "RPL_EBM", "RPL_SVM",  "RPL_HVM", n) %>% 
   mutate(n_ppps = ifelse(is.na(n), 0, as.numeric(n)),
          n_ppps_groups = case_when(n_ppps == 0 ~ "0",
@@ -88,29 +118,75 @@ all_tracts_ppps <-eji_data_fromCDC %>%
                                    n_ppps >100 ~ ">100"
          ),
          n_ppps_groups = factor(n_ppps_groups, levels = c("0", "1", "2", "3", "4", "5",
-                                                          "≤10", "≤20", "≤50", "≤100", ">100"))) %>%
+                                                          "≤10", "≤20", "≤50", 
+                                                          "≤100", ">100"
+         ))) %>%
   distinct()
 
+table(eji_w_ppps_eh249$n_ppps_groups)
+
+
+eji_w_ppps_salvatore <-eji_data_fromCDC %>% 
+  left_join(ppps_salvatore_count)  %>% 
+  select(1:geoid, contains("EJI"), "RPL_EBM", "RPL_SVM",  "RPL_HVM", n) %>% 
+  mutate(n_ppps = ifelse(is.na(n), 0, as.numeric(n)),
+         n_ppps_groups = case_when(n_ppps == 0 ~ "0",
+                                   n_ppps == 1 ~ "1",
+                                   n_ppps == 2 ~ "2",
+                                   n_ppps == 3 ~ "3",
+                                   n_ppps == 4 ~ "4",
+                                   n_ppps == 5 ~ "5",
+                                   n_ppps <= 10 ~ "≤10",
+                                   n_ppps <= 20 ~ "≤20",
+                                   n_ppps <= 50 ~ "≤50",
+                                   n_ppps > 50 ~ ">50"
+                                   # n_ppps <= 100 ~ "≤100",
+                                   # n_ppps >100 ~ ">100"
+         ),
+         n_ppps_groups = factor(n_ppps_groups, levels = c("0", "1", "2", "3", "4", "5",
+                                                          "≤10", "≤20", "≤50", ">50"
+                                                          # "≤100", ">100"
+                                                          ))) %>%
+  distinct()
+
+table(eji_w_ppps_salvatore$n_ppps_groups)
+
 #what is the avg number of ppps in each census tract?
-summary(all_tracts_ppps$n_ppps)
+summary(eji_w_ppps_eh249$n_ppps)
+
+summary(eji_w_ppps_salvatore$n_ppps)
 
 #how many census tracts in final dataset? 
-length(unique(all_tracts_ppps$geoid))
+length(unique(eji_w_ppps_eh249$geoid))
 #73,868
+#71,677 (12/21/22)
+
+length(unique(eji_w_ppps_salvatore$geoid))
+#71,677
+
+#they should match if we do a left_join rather than a full (full keeps all the ppps 
+# even if they don't have a census tract match in EJI)
 
 #how many ppps in final dataset? 
-sum(all_tracts_ppps$n_ppps)
+sum(eji_w_ppps_eh249$n_ppps)
 #83,692
+#110,691
 
-stopifnot(sum(all_tracts_ppps$n_ppps) == sum(ppps_count$n))
+#how many ppps in final dataset? 
+sum(eji_w_ppps_salvatore$n_ppps)
+#32,125 
 
+
+stopifnot(sum(eji_w_ppps_eh249$n_ppps) == sum(ppps_eh249_count$n))
+stopifnot(sum(eji_w_ppps_salvatore$n_ppps) == sum(ppps_salvatore_count$n))
 
 ################################################################################
 # 3. VISUALIZE SOME STUFF ####
 ################################################################################
 
 
-ggplot(all_tracts_ppps, aes(x = as.factor(n_ppps_groups), y = SPL_EJI)) +
+
+ggplot(eji_w_ppps_eh249, aes(x = as.factor(n_ppps_groups), y = SPL_EJI)) +
   ggforce::geom_sina(alpha = 0.5, color = "lightgrey", show.legend = TRUE) +
   geom_boxplot(width = 0.1, guides = FALSE, outlier.shape = NA, size = 1, color = "#3a3838") +
   xlab("# of Potential PFAS Point Sources") + 
@@ -120,13 +196,29 @@ ggplot(all_tracts_ppps, aes(x = as.factor(n_ppps_groups), y = SPL_EJI)) +
         panel.grid.major.x = element_line(color = "snow2"),
         strip.text = element_text(color = "black", face = "bold", size = 18),
         text = element_text(family = "Arial")
-  ) 
+  ) +
+  ggtitle("EJI with EH249 PFAS Point Sources")
+
+ggsave("eji census tracts with eh249 ppps boxplots.png", width = 18, height = 10, units = "in")
+
+ggplot(eji_w_ppps_salvatore, aes(x = as.factor(n_ppps_groups), y = SPL_EJI)) +
+  ggforce::geom_sina(alpha = 0.5, color = "lightgrey", show.legend = TRUE) +
+  geom_boxplot(width = 0.1, guides = FALSE, outlier.shape = NA, size = 1, color = "#3a3838") +
+  xlab("# of Potential PFAS Point Sources") + 
+  ylab("Sum of Env, Social, and Health Module Rankings") + 
+  theme_bw(base_size = 22) + 
+  theme(panel.grid.major.y = element_blank(),
+        panel.grid.major.x = element_line(color = "snow2"),
+        strip.text = element_text(color = "black", face = "bold", size = 18),
+        text = element_text(family = "Arial")
+  ) +
+  ggtitle("EJI with Salvatore subset PFAS Point Sources")
+
+ggsave("eji census tracts with salvatore ppps boxplots.png", width = 18, height = 10, units = "in")
 
 
-ggsave("all census tracts with ppps boxplots.png", width = 18, height = 10, units = "in")
 
-
-all_tracts_ppps %>% 
+eji_w_ppps_eh249 %>% 
   pivot_longer(names_to = "metric", values_to = "score", SPL_EJI:RPL_HVM) %>% 
   mutate(metric = factor(metric, levels = c("RPL_EJI", "SPL_EJI", "RPL_EBM", "RPL_HVM", "RPL_SVM"),
                          labels = c("EJI Rank", "Sum of all modules", "Env Burden Module",
@@ -142,27 +234,74 @@ all_tracts_ppps %>%
         panel.grid.major.x = element_line(color = "snow2"),
         strip.text = element_text(color = "black", face = "bold", size = 18),
         text = element_text(family = "Arial")
-  ) 
+  ) +
+  ggtitle("EJI with EH249 PFAS Point Sources")
 
-ggsave("all census tracts with ppps boxplots for diff variables.png", width = 18, height = 20, units = "in")
+ggsave("eji census tracts with eh249 ppps boxplots for diff variables.png", width = 18, height = 20, units = "in")
+
+eji_w_ppps_salvatore %>% 
+  pivot_longer(names_to = "metric", values_to = "score", SPL_EJI:RPL_HVM) %>% 
+  mutate(metric = factor(metric, levels = c("RPL_EJI", "SPL_EJI", "RPL_EBM", "RPL_HVM", "RPL_SVM"),
+                         labels = c("EJI Rank", "Sum of all modules", "Env Burden Module",
+                                    "Health Vulnerability Module", "Social Vulnerability Module"))) %>% 
+  ggplot(aes(x = as.factor(n_ppps_groups), y = score)) +
+  ggforce::geom_sina(alpha = 0.5, color = "lightgrey", show.legend = TRUE) +
+  geom_boxplot(width = 0.1, guides = FALSE, outlier.shape = NA, size = 1, color = "#3a3838") +
+  xlab("# of Potential PFAS Point Sources") + 
+  ylab("Sum of Env, Social, and Health Module Rankings") + 
+  facet_wrap(~metric, ncol = 1, scales = "free_y") +
+  theme_bw(base_size = 22) + 
+  theme(panel.grid.major.y = element_blank(),
+        panel.grid.major.x = element_line(color = "snow2"),
+        strip.text = element_text(color = "black", face = "bold", size = 18),
+        text = element_text(family = "Arial")
+  ) +
+  ggtitle("EJI with salvatore PFAS Point Sources")
+
+ggsave("eji census tracts with salvatore ppps boxplots for diff variables.png", width = 18, height = 20, units = "in")
+
 
 
 ### group by dataset
 
-ppps_count_dataset <- ppps_census %>% 
+
+ppps_salvatore_count_dataset <- ppps_salvatore_tract_df %>% 
   group_by(geoid, dataset) %>% 
-  count()
+  count() %>% 
+  filter(!is.na(geoid))
 
+ppps_eh249_count_dataset <- ppps_eh249_tract_df %>% 
+  group_by(geoid, dataset) %>% 
+  count() %>% 
+  filter(!is.na(geoid))
 
-all_tracts_ppps_dataset <-eji_data_fromCDC %>% 
-  select(1:geoid, contains("EJI")) %>% 
-  full_join(ppps_count_dataset)  %>% 
-  distinct() %>% 
-  mutate(dataset = case_when(is.na(dataset) ~ "all ppps",
-                             TRUE ~ dataset)) %>% 
-  pivot_wider(names_from = dataset, values_from = n, values_fill = 0) %>% 
-  pivot_longer(names_to = "dataset", values_to = "n", `all ppps`:superfund_sf) %>% 
-  select(1:geoid, contains("EJI"), dataset, n) %>% 
+eji_w_ppps_salvatore_dataset <-eji_data_fromCDC %>% 
+  left_join(ppps_salvatore_count_dataset)  %>% 
+  select(1:geoid, dataset, contains("EJI"), "RPL_EBM", "RPL_SVM",  "RPL_HVM", n) %>% 
+  mutate(n_ppps = ifelse(is.na(n), 0, as.numeric(n)),
+         n_ppps_groups = case_when(n_ppps == 0 ~ "0",
+                                   n_ppps == 1 ~ "1",
+                                   n_ppps == 2 ~ "2",
+                                   n_ppps == 3 ~ "3",
+                                   n_ppps == 4 ~ "4",
+                                   n_ppps == 5 ~ "5",
+                                   n_ppps <= 10 ~ "≤10",
+                                   n_ppps <= 20 ~ "≤20",
+                                   n_ppps <= 50 ~ "≤50",
+                                   n_ppps > 50 ~ ">50"
+                                   # n_ppps <= 100 ~ "≤100",
+                                   # n_ppps >100 ~ ">100"
+         ),
+         n_ppps_groups = factor(n_ppps_groups, levels = c("0", "1", "2", "3", "4", "5",
+                                                          "≤10", "≤20", "≤50", ">50"
+                                                          # "≤100", ">100"
+         ))) %>%
+  distinct()
+  
+
+eji_w_ppps_eh249_dataset <-eji_data_fromCDC %>% 
+  left_join(ppps_eh249_count_dataset)  %>% 
+  select(1:geoid, dataset, contains("EJI"), "RPL_EBM", "RPL_SVM",  "RPL_HVM", n) %>% 
   mutate(n_ppps = ifelse(is.na(n), 0, as.numeric(n)),
          n_ppps_groups = case_when(n_ppps == 0 ~ "0",
                                    n_ppps == 1 ~ "1",
@@ -177,24 +316,27 @@ all_tracts_ppps_dataset <-eji_data_fromCDC %>%
                                    n_ppps >100 ~ ">100"
          ),
          n_ppps_groups = factor(n_ppps_groups, levels = c("0", "1", "2", "3", "4", "5",
-                                                          "≤10", "≤20", "≤50", "≤100", ">100")))
+                                                          "≤10", "≤20", "≤50", 
+                                                          "≤100", ">100"
+         ))) %>%
+  distinct()
 
-all_tracts_together_w_datasets_too <- all_tracts_ppps %>% 
-  mutate(dataset = "all ppps") %>% 
-  bind_rows(all_tracts_ppps_dataset) %>% 
-  mutate( dataset = factor(dataset, levels = c("all ppps", "airports_sf", "epastewardship_county_sf",
-                                               "facilities_sf", "fed_agencies_sf", "fire_sf", "production_sf",
-                                               "spills_sf", "superfund_sf"),
-                           labels = c("All Potential\nPFAS Point Sources", "14 CFR Part\n139 Airports",
-                                      "EPA Stewardship Program\nParticipating Facility",
-                                      "Facilities in Industries\nthat May be Handling PFAS",
-                                      "Federal Agency Locations with\nKnown or Suspected\nPFAS Detections",
-                                      "Fire Training Sites", "Facilities that Manufacture\nor Import PFAS",
-                                      "Known PFAS\nSpills/Release Incidents", "Superfund Sites\nwith PFAS Detections")))
+# 
+# all_tracts_together_w_datasets_too <- all_tracts_ppps %>% 
+#   mutate(dataset = "all ppps") %>% 
+#   bind_rows(all_tracts_ppps_dataset) %>% 
+#   mutate( dataset = factor(dataset, levels = c("all ppps", "airports_sf", "epastewardship_county_sf",
+#                                                "facilities_sf", "fed_agencies_sf", "fire_sf", "production_sf",
+#                                                "spills_sf", "superfund_sf"),
+#                            labels = c("All Potential\nPFAS Point Sources", "14 CFR Part\n139 Airports",
+#                                       "EPA Stewardship Program\nParticipating Facility",
+#                                       "Facilities in Industries\nthat May be Handling PFAS",
+#                                       "Federal Agency Locations with\nKnown or Suspected\nPFAS Detections",
+#                                       "Fire Training Sites", "Facilities that Manufacture\nor Import PFAS",
+#                                       "Known PFAS\nSpills/Release Incidents", "Superfund Sites\nwith PFAS Detections")))
 
 
-ggplot(all_tracts_together_w_datasets_too %>% 
-         filter(dataset != "All Potential\nPFAS Point Sources"), aes(x = n_ppps_groups, y = SPL_EJI)) +
+ggplot(eji_w_ppps_eh249_dataset, aes(x = n_ppps_groups, y = SPL_EJI)) +
   ggforce::geom_sina(alpha = 0.5, color = "lightgrey") +
   geom_boxplot(width = 0.1, guides = FALSE, outlier.shape = NA, size = 1, color = "#3a3838") +
   xlab("# of Potential PFAS Point Sources") + 
@@ -205,10 +347,31 @@ ggplot(all_tracts_together_w_datasets_too %>%
         panel.grid.major.x = element_line(color = "snow2"),
         strip.text = element_text(color = "black", face = "bold", size = 18),
         text = element_text(family = "Arial")
-  ) 
+  ) + 
+  ggtitle("EJI with EH249 PFAS Point Sources")
 
 
-ggsave("all census tracts with ppps boxplots by dataset.png", width = 18, height = 10, units = "in")
+
+ggsave("eji census tracts with eh249 ppps boxplots by dataset.png", width = 18, height = 10, units = "in")
+
+
+
+ggplot(eji_w_ppps_salvatore_dataset, aes(x = n_ppps_groups, y = SPL_EJI)) +
+  ggforce::geom_sina(alpha = 0.5, color = "lightgrey") +
+  geom_boxplot(width = 0.1, guides = FALSE, outlier.shape = NA, size = 1, color = "#3a3838") +
+  xlab("# of Potential PFAS Point Sources") + 
+  ylab("Sum of Env, Social, and Health Module Rankings") + 
+  facet_wrap(~dataset, scales = "free_x", ncol = 2) + 
+  theme_bw(base_size = 22) + 
+  theme(panel.grid.major.y = element_blank(),
+        panel.grid.major.x = element_line(color = "snow2"),
+        strip.text = element_text(color = "black", face = "bold", size = 18),
+        text = element_text(family = "Arial")
+  ) + 
+  ggtitle("EJI with salvatore PFAS Point Sources")
+
+
+ggsave("eji census tracts with salvatore ppps boxplots by dataset.png", width = 18, height = 10, units = "in")
 
 
 ################################################################################
