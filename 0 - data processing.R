32b54f1f9782a9a05e8cb8e46488f38dde885309
amandabhernandez <amandabhernandez@gmail.com>
2022-12-21 13:07:20 -0600

    add naics list, change census pull to match EJI 5yr estimate, prep data for conservative ppps analysis

--- 0 - data processing.R
+++ 0 - data processing.R
@@ -137,22 +137,93 @@ superfund_sf <- superfund %>%
 # 1f. FACILITIES  ####
 ################################################################################
 
+naics_1 <- data.frame(naics = c( "313320 Fabric Coating Mills bcdefghik 380",
+                                 "325510 Paint and Coating Manufacturing abcdefhik 2100",
+                                 "322220 Paper Bag and Coated and Treated Paper Manufacturing bcdefghi 0",
+                                 "313210 Broadwoven Fabric Mills bcdefhk 484",
+                                 "322121 Paper (except Newsprint) Mills bcdefhk 610",
+                                 "332813 Electroplating, Plating, Polishing, Anodizing, and Coloring bcdefhi 5642",
+                                 "324110 Petroleum Refineries abcdehk 594",
+                                 "325612 Polish and Other Sanitation Good Manufacturing abdefhk 673",
+                                 "334413 Semiconductor and Related Device Manufacturing bcdefi 1552",
+                                 "326113 Unlaminated Plastics Film and Sheet (except Packaging) Manufacturing bdefhi 597",
+                                 "332812 Metal Coating, Engraving (except Jewelry and Silverware), and Allied Services to Manufacturers bcefgi 3587",
+                                 "333318 Other Commercial and Service Industry Machinery Manufacturing bdefgh 0",
+                                 "334419 Other Electronic Component Manufacturing bcdefg 1663",
+                                 "562212 Solid Waste Landfill cdefjk 4765",
+                                 "325199 All Other Basic Organic Chemical Manufacturing abdefi 1847",
+                                 "323111 Commercial Printing (except Screen and Books) bcdhk 4226",
+                                 "313110 Fiber, Yarn, and Thread Mills bcefh 0",
+                                 "314110 Carpet and Rug Mills bdhik 189",
+                                 "316110 Leather and Hide Tanning and Finishing bdefg 276",
+                                 "325211 Plastics Material and Resin Manufacturing bdgik 2141",
+                                 "324191 Petroleum Lubricating Oil and Grease Manufacturing abcdh 919",
+                                 "325998 All Other Miscellaneous Chemical Product and Preparation Manufacturing aefgk 2598",
+                                 "562211 Hazardous Waste Treatment and Disposal acefj 1357",
+                                 "562213 Solid Waste Combustors and Incinerators acefj 407",
+                                 "313310 Textile and Fabric Finishing Mills bcdh 0",
+                                 "322219 Other Paperboard Container Manufacturing bcdh 0",
+                                 "323120 Support Activities for Printing bcdh 0",
+                                 "313220 Narrow Fabric Mills and Schiffli Machine Embroidery cefk 0",
+                                 "313230 Nonwoven Fabric Mills bchk 239",
+                                 "322130 Paperboard Mills cefk 258")
+)
+
+
+naics_2 <- data.frame(naics = c("332999 All Other Miscellaneous Fabricated Metal Product Manufacturing bdgh 4799 ",
+                                "424690 Other Chemical and Allied Products Merchant Wholesalers bdhj 1698",
+                                "314910 Textile Bag and Canvas Mills befi 0",
+                                "326112 Plastics Packaging Film and Sheet (including Laminated) Manufacturing defi 350",
+                                "335999 All Other Miscellaneous Electrical Equipment and Component Manufacturing befg 877",
+                                "562112 Hazardous Waste Collection cefj 2561",
+                                "562219 Other Nonhazardous Waste Treatment and Disposal cefj 744",
+                                "325611 Soap and Other Detergent Manufacturing abeh 1012")
+)
+
+naics_salvatore <- bind_rows(naics_1, naics_2) %>% 
+  separate(naics, into = c("naics_code", "description"), sep = "(?!\\d) ")
+
 facilities <- read_xlsx("PFAS Point Source Data/Facilities_in_industries_that_may_be_handling_PFAS_01-03-2022.xlsx",
                         sheet = 2)
 
-facilities_sf <- facilities %>% 
+facilities_naics <- facilities %>% 
+  mutate(frs_id = str_extract(`ECHO Facility Report`, "(?<=fid=)\\d*"))  %>% 
+  select(frs_id, contains("NAICS")) %>% 
+  separate_rows(CAA_NAICS, sep = " ") %>% 
+  separate_rows(CWA_NAICS, sep = " ") %>% 
+  separate_rows(RCRA_NAICS, sep = " ") %>% 
+  filter(CAA_NAICS %in% c(naics_salvatore$naics_code) | 
+           CWA_NAICS %in% c(naics_salvatore$naics_code) |
+           RCRA_NAICS %in% c(naics_salvatore$naics_code))
+
+facilities_eh249 <- facilities %>% 
   mutate(frs_id = str_extract(`ECHO Facility Report`, "(?<=fid=)\\d*")) %>% 
   filter(!Industry %in% c("Airports", "Fire Training", "National Defense")) %>% 
   filter(!is.na(Latitude)) %>% 
-  filter(TRI_FLAG == "N") %>% 
+  filter(TRI_FLAG == "N")
+
+facilities_salvatore <- facilities %>% 
+  mutate(frs_id = str_extract(`ECHO Facility Report`, "(?<=fid=)\\d*")) %>% 
+  filter(!Industry %in% c("Airports", "Fire Training", "National Defense")) %>% 
+  filter(!is.na(Latitude)) %>% 
+  filter(frs_id %in% facilities_naics$frs_id)%>% 
+  filter(TRI_FLAG == "N")
+
+setdiff(facilities_salvatore$frs_id, facilities_eh249_inclusion$frs_id)
+
+facilities_sf_eh249 <- facilities_eh249 %>% 
   sf::st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>% 
   rename(loc_name = Facility) %>% 
   clean_names() %>% 
   select(any_of(keepcols)) 
 
 
-check_facilities <- facilities_sf %>% 
-  filter(!state %in% stateabbnamekey$stateabb)
+facilities_sf_salvatore <- facilities_salvatore %>% 
+  sf::st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>% 
+  rename(loc_name = Facility) %>% 
+  clean_names() %>% 
+  select(any_of(keepcols)) 
+
 
 
 ################################################################################
@@ -178,12 +249,12 @@ spills_sf <- spills %>%
 
 production <- read_xlsx("PFAS Point Source Data/PFAS_Production_Data_02-28-2022_0.xlsx", sheet = 2)
 
-table(production$Identifier %in% c(facilities_id$frs_id))
+table(production$Identifier %in% c(facilities_salvatore$frs_id))
 
 production_sf <- production %>% 
   mutate(across(c(Latitude, Longitude), as.numeric)) %>% 
   filter(!is.na(Latitude)) %>% 
-  filter(!Identifier %in% facilities_id$frs_id) %>% 
+  filter(!Identifier %in% facilities_salvatore$frs_id) %>% 
   sf::st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>% 
   rename(loc_name = `Facility Name`) %>% 
   clean_names() %>% 
@@ -194,19 +265,33 @@ production_sf <- production %>%
 # 2. JOIN TOGETHER ####
 ################################################################################
 
-all_ppps <- bind_rows(lst(epastewardship_county_sf, 
+all_ppps_eh249 <- bind_rows(lst(epastewardship_county_sf, 
                           fire_sf, airports_sf,
                           fed_agencies_sf, superfund_sf, 
-                          facilities_sf, spills_sf, production_sf), .id = "dataset") 
+                          facilities_sf_eh249, spills_sf, production_sf), .id = "dataset") 
 
-nrow(all_ppps)
+
+all_ppps_salvatore <- bind_rows(lst(epastewardship_county_sf, 
+                                fire_sf, airports_sf,
+                                fed_agencies_sf, superfund_sf, 
+                                facilities_salvatore, spills_sf, production_sf), .id = "dataset") 
+
+# nrow(all_ppps_eh249)
 
 ################################################################################
 # 3. LOAD EJI  ####
 ################################################################################
 
 ### load csv from CDC site
-eji_data_fromCDC <- read_csv("eji/DataRecords.csv")
+eji_data_fromCDC <- read_csv("eji/DataRecords.csv") %>% 
+  filter(!is.na(RPL_EJI))
+
+# table(eji_data_fromCDC$StateAbbr, is.na(eji_data_fromCDC$RPL_EJI), useNA = "ifany")
+# tabyl(eji_data_fromCDC, StateAbbr, statefp) %>% View
+# 
+# eji_data_fromCDC %>% 
+#   filter(is.na(StateAbbr)) %>% 
+#   View()
 
 
 ################################################################################
@@ -215,16 +300,36 @@ eji_data_fromCDC <- read_csv("eji/DataRecords.csv")
 
 p_load(tigris)
 
-# state_list <- c("MA")
 state_list <- unique(stateabbnamekey$stateabb)
 
-census_list <- list()
+
+
+
+#census_data_bind <-  rbind_tigris(census_list)
+
+
+fips <- tigris::fips_codes
+
+
+p_load(tidycensus)
+Sys.getenv("CENSUS_API_KEY")
+census_api_key("f276739b31b176620b81010095385ce4e5608b5f")
+
+
+tidycensus_list_5yr_2019 <- list()
 
 for(i in state_list){
-  census_list[[i]] <- tracts(state = i)
+  tidycensus_list_5yr_2019[[i]] <- tidycensus::get_acs(
+    geography = "tract",
+    variables = "B19013_001",
+    state = i,
+    year = 2019, 
+    geometry = TRUE
+  )
 }
 
-census_data_bind <-  rbind_tigris(census_list)
+
+ 
 
 
 ################################################################################
@@ -232,11 +337,38 @@ census_data_bind <-  rbind_tigris(census_list)
 ################################################################################
 
 
-census_tracts <- rbind_tigris(census_list) %>% 
-  janitor::clean_names() 
 
-census_tracts_eji <- census_tracts %>% 
-  left_join(eji_data_fromCDC %>% select(1:geoid, contains("EJI")))
+## 5yr estimate tracts
+
+tidy_tracts <- bind_rows(tidycensus_list_5yr_2019) %>%   
+  janitor::clean_names() %>% 
+  select(-variable, -estimate, -moe)
+
+eji_spatial_tidy <- tidy_tracts %>% 
+  left_join(eji_data_fromCDC %>% select(1:geoid, contains("EJI"))) %>% 
+  left_join(fips, by = c( "statefp" = "state_code", "countyfp" = "county_code" )) 
+#73128 (full)
+#73128 (left)
+
+# stopifnot()
+
+eji_spatial_tidy %>%
+  filter(is.na(name)) %>% 
+  View()
+
+
+eji_spatial_tidy %>%
+  filter(is.na(RPL_EJI)) %>% 
+  View()
+
+
+
+table(is.na(eji_spatial_tidy$RPL_EJI))
+#1379 where EJI is empty, but exists in census data, going to keep so we know that
+#ppps are matching somewhere
+
+#get stuff that's missing -- look at specific case
+
 
 
 ################################################################################
@@ -244,14 +376,19 @@ census_tracts_eji <- census_tracts %>%
 ################################################################################
 
 
-st_crs(census_tracts)
-st_crs(all_ppps)
-census_tracts_transformed<-st_transform(census_tracts_eji,st_crs(all_ppps))
+st_crs(eji_spatial_tidy)
+st_crs(all_ppps_salvatore)
+census_tracts_transformed<-st_transform(eji_spatial_tidy,st_crs(all_ppps_salvatore))
 
 
-sites_census_tract <- all_ppps %>% 
-  sf::st_join(census_tracts_eji, join = st_intersects)
+ppps_salvatore_tract <- all_ppps_salvatore %>% 
+  sf::st_join(census_tracts_transformed, join = st_intersects)
 
+ppps_eh249_tract <- all_ppps_eh249 %>% 
+  sf::st_join(census_tracts_transformed, join = st_intersects)
+
+
+#add naics list, change census pull to match EJI 5yr estimate
 
 # tm_shape(census_tracts) +
 #   tm_polygons("RPL_EJI", palette="PuBu", alpha = 0.5) + 
@@ -261,7 +398,7 @@ sites_census_tract <- all_ppps %>%
 
 
 #save as RData file so it can be opened in next script 
-save(census_tracts_eji, sites_census_tract, 
+save(eji_spatial_tidy, ppps_salvatore_tract, ppps_eh249_tract,
      file = "census_eji_all_ppps.RData")
 
 ################################################################################
@@ -309,6 +446,12 @@ write_csv(table1, "summary of ppps merges.csv")
 
 
 
+################################################################################
+#<3 GARBAGE <3 <3    ####
+################################################################################
+
+
+
 # 
 # ggplot(table1 %>% 
 #          pivot_longer(names_to = "count", values_to = "n", n:n_inEJI),
@@ -342,3 +485,90 @@ write_csv(table1, "summary of ppps merges.csv")
 # 
 # 
 # ggsave("ppps_match_recovery.png", width = 25, height = 10, units = "in")
+
+
+# 
+# census_list <- list()
+# 
+# for(i in state_list){
+#   census_list[[i]] <- tracts(state = i, year = "2021")
+# }
+# 
+# census_list_2020 <- list()
+# 
+# for(i in state_list){
+#   census_list_2020[[i]] <- tracts(state = i, year = "2020")
+# }
+# 
+# 
+# census_list_2019 <- list()
+# 
+# for(i in state_list){
+#   census_list_2019[[i]] <- tracts(state = i, year = "2019")
+# }
+# 
+# 
+# 
+# census_list_2018 <- list()
+# 
+# for(i in state_list){
+#   census_list_2018[[i]] <- tracts(state = i, year = "2018")
+# }
+# 
+
+## 2021
+census_tracts <- rbind_tigris(census_list) %>% 
+  janitor::clean_names() 
+
+eji_spatial <- census_tracts %>% 
+  full_join(eji_data_fromCDC %>% select(1:geoid, contains("EJI"))) %>% 
+  left_join(fips, by = c( "statefp" = "state_code", "countyfp" = "county_code" )) 
+#84476
+#97066
+
+
+## 2020
+census_tracts_2020 <- rbind_tigris(census_list_2020) %>% 
+  janitor::clean_names() 
+
+eji_spatial_2020 <- census_tracts_2020 %>% 
+  full_join(eji_data_fromCDC %>% select(1:geoid, contains("EJI"))) %>% 
+  left_join(fips, by = c( "statefp" = "state_code", "countyfp" = "county_code" )) 
+
+length(unique(eji_spatial_2020$geoid[which(is.na(eji_spatial_2020$RPL_EJI))]))
+
+
+
+## 2019 is best match so far -- makes sense since the ACS data is 2014-2019 5 yr estimates
+census_tracts_2019 <- rbind_tigris(census_list_2019) %>% 
+  janitor::clean_names() 
+
+eji_spatial_2019 <- census_tracts_2019 %>% 
+  full_join(eji_data_fromCDC %>% select(1:geoid, contains("EJI"))) %>% 
+  left_join(fips, by = c( "statefp" = "state_code", "countyfp" = "county_code" )) 
+
+length(unique(eji_spatial_2019$geoid[which(is.na(eji_spatial_2019$RPL_EJI))]))
+#2416
+
+## 2018
+census_tracts_2018 <- rbind_tigris(census_list_2018) %>% 
+  janitor::clean_names() 
+
+eji_spatial_2018 <- census_tracts_2018 %>% 
+  full_join(eji_data_fromCDC %>% select(1:geoid, contains("EJI"))) %>% 
+  left_join(fips, by = c( "statefp" = "state_code", "countyfp" = "county_code" )) 
+
+length(unique(eji_spatial_2018$geoid[which(is.na(eji_spatial_2018$RPL_EJI))]))
+#2416
+
+
+cr <- eji_spatial %>%
+  filter(!is.na(namelsad)) %>% 
+  # filter(is.na(RPL_EJI))  %>% 
+  # filter(statefp == "01" & countyfp == "001") %>% 
+  mutate(flag = case_when(is.na(RPL_EJI) ~ "!"))
+
+library(tmap)
+
+tm_shape(cr) + 
+  tm_polygons("flag")
