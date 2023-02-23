affb8483f39e00037fc21eab080d5a30ef00ce83
amandabhernandez <amandabhernandez@gmail.com>
2023-02-23 10:58:57 -0500

    add custom plot functions

--- scripts/0 - setup.R
+++ scripts/0 - setup.R
@@ -9,7 +9,7 @@
 ################################################################################
 
 library(pacman)
-p_load(tidyverse, readxl, janitor, sf, broom, flextable)
+p_load(tidyverse, readxl, janitor, sf, broom, flextable, tmap)
 
 ################################################################################
 #  SET WD  ####
@@ -17,7 +17,28 @@ p_load(tidyverse, readxl, janitor, sf, broom, flextable)
 
 setwd(here::here())
 
+################################################################################
+#  OPTIONS  ####
+################################################################################
+
+theme_set(theme_bw(base_size= 22))
 
 ################################################################################
 #  FUNCTIONS  ####
 ################################################################################
+
+sina_boxplot <- function(ggobj){
+    ggobj + 
+    ggforce::geom_sina(alpha = 0.5, color = "lightgrey", show.legend = TRUE) +
+    geom_boxplot(width = 0.1, guides = FALSE, outlier.shape = NA, size = 1, color = "#3a3838") 
+}
+
+
+density_plot <- function(ggobj){
+  ggobj + 
+    geom_histogram(aes(y = after_stat(density)),
+                   color = "black",
+                   fill = "white",
+                   bins = 30) +
+    geom_density(fill = "red", alpha = 0.25)
+}
